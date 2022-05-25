d872fe538d4b1e3587726c784dd6356cc27f5c3f
Alfredo Di Napoli <alfredo@well-typed.com>
2022-05-25 12:37:03 +0200

    General improvements

--- src/System/Remote/Monitoring/Prometheus.hs
+++ src/System/Remote/Monitoring/Prometheus.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
@@ -18,7 +20,6 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Trans.State.Strict
 import qualified Data.HashMap.Strict as HMap
 import qualified Data.Map.Strict as Map
-import           Data.Monoid
 import Lens.Micro.TH
 import qualified Data.Text as T
 import qualified System.Metrics as EKG
@@ -53,18 +54,17 @@ defaultOptions l = AdapterOptions l Nothing 15
 registerEKGStore :: MonadIO m => EKG.Store -> AdapterOptions -> RegistryT m ()
 registerEKGStore store opts = RegistryT $ StateT $ \_ -> do
   (r, mmap) <- liftIO $ toPrometheusRegistry' store opts
-  liftIO $ forkIO $ do
-    let loop = forever $ do
-                 threadDelay (_samplingFrequency opts * 10^6)
-                 updateMetrics store opts mmap
-                 loop
+  void $ liftIO $ forkIO $ do
+    let loop = do threadDelay (_samplingFrequency opts * (1_000_000 :: Int))
+                  updateMetrics store opts mmap
+                  loop
     loop
-  return ((), r)
+  pure ((), r)
 
 --------------------------------------------------------------------------------
 toPrometheusRegistry' :: EKG.Store -> AdapterOptions -> IO (Prometheus.Registry, MetricsMap)
 toPrometheusRegistry' store opts = do
-  let registry = Prometheus.new
+  let !registry = Prometheus.new
   samples <- EKG.sampleAll store
   foldM (mkMetric opts) (registry, Map.empty) (HMap.toList samples)
 
@@ -74,19 +74,19 @@ toPrometheusRegistry store opts = fst <$> toPrometheusRegistry' store opts
 
 --------------------------------------------------------------------------------
 mkMetric :: AdapterOptions -> (Prometheus.Registry, MetricsMap) -> (T.Text, EKG.Value) -> IO (Prometheus.Registry, MetricsMap)
-mkMetric AdapterOptions{..} (oldRegistry, mmap) (key, value) = do
+mkMetric AdapterOptions{..} (!oldRegistry, !mmap) (!key, !value) = do
   let k = mkKey _namespace key
   case value of
    EKG.Counter c -> do
      (counter, newRegistry) <- Prometheus.registerCounter k _labels oldRegistry
      Counter.add (fromIntegral c) counter
-     return $! (newRegistry, Map.insert k (C counter) $! mmap)
+     pure $! (newRegistry, Map.insert k (C counter) $! mmap)
    EKG.Gauge g   -> do
      (gauge, newRegistry) <- Prometheus.registerGauge k _labels oldRegistry
      Gauge.set (fromIntegral g) gauge
-     return $! (newRegistry, Map.insert k (G gauge) $! mmap)
-   EKG.Label _   -> return $! (oldRegistry, mmap)
-   EKG.Distribution _ -> return $! (oldRegistry, mmap)
+     pure $! (newRegistry, Map.insert k (G gauge) $! mmap)
+   EKG.Label _   -> pure $! (oldRegistry, mmap)
+   EKG.Distribution _ -> pure $! (oldRegistry, mmap)
 
 --------------------------------------------------------------------------------
 updateMetrics :: EKG.Store -> AdapterOptions -> MetricsMap -> IO ()
@@ -102,15 +102,17 @@ mkKey mbNs k =
 
 --------------------------------------------------------------------------------
 updateMetric :: AdapterOptions -> MetricsMap -> (T.Text, EKG.Value) -> IO MetricsMap
-updateMetric AdapterOptions{..} mmap (key, value) = do
+updateMetric AdapterOptions{..} mmap (!key, !value) = do
   let k = mkKey _namespace key
-  case liftM2 (,) (Map.lookup k mmap) (Just value) of
-    Just (C counter, EKG.Counter c)  -> do
-      (Counter.CounterSample oldCounterValue) <- Counter.sample counter
-      let slack = c - fromIntegral oldCounterValue
-      when (slack >= 0) $ Counter.add (fromIntegral slack) counter
-      return $! mmap
-    Just (G gauge,   EKG.Gauge g) -> do
-      Gauge.set (fromIntegral g) gauge
-      return $! mmap
-    _ -> return mmap
+  case Map.lookup k mmap of
+    Just (C counter)
+     | EKG.Counter c <- value
+     -> do (Counter.CounterSample oldCounterValue) <- Counter.sample counter
+           let slack = c - fromIntegral oldCounterValue
+           when (slack >= 0) $ Counter.add (fromIntegral slack) counter
+           pure $! mmap
+    Just (G gauge)
+      | EKG.Gauge g <- value
+      -> do Gauge.set (fromIntegral g) gauge
+            pure $! mmap
+    _ -> pure mmap
